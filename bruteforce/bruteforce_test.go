5c820957eedbf69d36bab00b0df379eb49562dfd
Ananthu <ananthusudhikumar@gmail.com>
2022-05-25 09:04:09 +0530

    test(database): seed data

---
+++ bruteforce/bruteforce_test.go
@@ -0,0 +1,80 @@
+package bruteforce
+
+import (
+	"avala/common"
+	"fmt"
+	"gorm.io/gorm"
+	"math"
+	"sync"
+)
+
+const batchSize = 0x10_000
+const databaseBatch = 0x1_000
+const upperLimit = 0xfffff
+
+// createBatch processes batches of data that will be inserted to the database.
+func createBatch(offset int, wg *sync.WaitGroup, db *gorm.DB) {
+
+	start := offset * batchSize
+	end := start + batchSize
+
+	hexList := make([]common.HexCounter, batchSize)
+
+	if end > upperLimit {
+		newLimit := end - upperLimit
+		hexList = hexList[:batchSize-newLimit+1]
+	}
+
+	for i := range hexList {
+		currentHex := start + i
+
+		if currentHex > upperLimit {
+			break
+		}
+
+		hexList[i] = common.HexCounter{
+			Hex:   currentHex,
+			Count: 0,
+		}
+	}
+
+	tx := db.Begin()
+	tx.CreateInBatches(hexList, databaseBatch).
+		Commit()
+	fmt.Printf("Offset %v done\n", offset)
+
+	wg.Done()
+}
+
+// Seed adds all the possible data values to the db.
+func Seed() {
+	iteration := int(math.Ceil(float64(upperLimit) / batchSize))
+
+	wg := sync.WaitGroup{}
+
+	wg.Add(iteration + 1)
+	ch := make(chan int, iteration)
+
+	for thread := 0; thread < 16; thread++ {
+		db := common.GetDb()
+
+		go func(ch <-chan int) {
+			for i := range ch {
+				createBatch(i, &wg, db)
+			}
+		}(ch)
+	}
+
+	go func(ch chan<- int) {
+		defer func() {
+			close(ch)
+		}()
+
+		for i := 0; i < iteration; i++ {
+			ch <- i
+		}
+		wg.Done()
+	}(ch)
+
+	wg.Wait()
+}
